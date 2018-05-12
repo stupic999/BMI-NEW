f601b4cd49e4b2fab6d51d10e3f1da3a5c76cfe2
DESKTOP-H468MRI\Moucy Cat <aierzhiguang2@gmail.com>
2018-05-12 22:21:03 +0800

    修改了if和else if上方的空格

--- BMI/BMI/Program.cs
+++ BMI/BMI/Program.cs
@@ -23,7 +23,6 @@ namespace BMI
 
             // 宣告BMI的公式 
             double BMI = weight * 10000 / (height * height);
-
             if (BMI < 18.5)
             {
                 Console.WriteLine("體重過輕");
@@ -32,42 +31,36 @@ namespace BMI
                 Console.Beep();
                 Console.BackgroundColor = ConsoleColor.Red;
             }
-
             else if (BMI >= 18.5 && BMI < 24)
             {
                 Console.WriteLine("恭喜你，非常正常^^");
 
                 Console.BackgroundColor = ConsoleColor.Green;
             }
-
             else if (BMI >= 24 && BMI < 27)
             {
                 Console.WriteLine("你有點過重哦");
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.Beep();
             }
-
             else if (BMI >= 27 && BMI < 30)
             {
                 Console.WriteLine("輕度肥胖");
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.Beep();
             }
-
             else if (BMI >= 30 && BMI < 35)
             {
                 Console.WriteLine("中度肥胖");
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.Beep();
             }
-
             else
             {
                 Console.WriteLine("重度肥胖");
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.Beep();
             }
-
             Console.WriteLine("你的BMI數值是" + BMI.ToString());
 
             // 按任意按鈕即可退出 
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintText("123", ConsoleColor.Red, ConsoleColor.Blue);
+            Console.ReadKey();
+        }
+
+        static void PrintText(string text, ConsoleColor textColor, ConsoleColor bgColor)
+        {
+            Console.BackgroundColor = bgColor;
+            Console.ForegroundColor = textColor;
+            Console.Write(text);
+        }
+    }
+}
+
